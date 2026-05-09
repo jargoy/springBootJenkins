f6860959d6c118bf01361dfbf2d8c9580d4f7e9b
unknown <yograjrajput027@gmail.com>
2026-05-09 19:10:01 +0530

    first commit

---
+++ src/main/java/com/springBootJenkins/SpringBootJenkinsApplication.java
@@ -0,0 +1,27 @@
+package com.springBootJenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init()
+	{
+		logger.info("Application started...");
+	}
+	
+	public static void main(String[] args) {
+		
+		logger.info("Application is executed");
+		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+	}
+
+}
